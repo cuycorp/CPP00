94e94c2e2feb89a339637b62dca3bc837679a5a3
cuycorp <cuycorp@gmail.com>
2025-09-22 14:48:00 +0200

    fix makeWithdrawal

--- ex02/src/Account.cpp
+++ ex02/src/Account.cpp
@@ -28,6 +28,7 @@ Account::Account(int initial_deposit)
 Account::~Account(void)
 {
 	Account::_nbAccounts -= 1;
+	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
 	return ;
 }
@@ -73,7 +74,7 @@ bool Account::makeWithdrawal(int withdrawal)
 		return (true);
 	} 
 		_displayTimestamp();
-		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ":refused" << std::endl;
+		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:refused" << std::endl;
 		return (false);
 }
 
